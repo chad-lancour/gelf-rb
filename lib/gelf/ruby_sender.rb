f6f611f5b5e87097c43b364f3ae2ef953810eb54
tbdev <chad.lancour@oracle.com>
2014-03-16 23:34:09 -0700

    open socket, close socket, repeat.

--- lib/gelf/ruby_sender.rb
+++ lib/gelf/ruby_sender.rb
@@ -6,15 +6,17 @@ module GELF
     def initialize(addresses)
       @addresses = addresses
       @i = 0
-      @socket = UDPSocket.open
+      #@socket = UDPSocket.open
     end
 
     def send_datagrams(datagrams)
       host, port = @addresses[@i]
       @i = (@i + 1) % @addresses.length
+      socket = UDPSocket.open
       datagrams.each do |datagram|
-        @socket.send(datagram, 0, host, port)
+        socket.send(datagram, 0, host, port)
       end
+      socket.close
     end
   end
 end
